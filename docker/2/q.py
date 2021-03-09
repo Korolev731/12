3bf258941aecb7c99d2794db61d6aeae05e65190
IKorolev <salix731@gmail.com>
2021-03-09 17:37:28 +0100

    05

--- docker/2/q.py
+++ docker/2/q.py
@@ -3,7 +3,7 @@ import socket
 # Configure socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(('', 8080))
+server.bind(('', 6589))
 server.listen()
 
 # Listen requests
@@ -11,5 +11,5 @@ while True:
     client_socket, addr = server.accept()
     request = client_socket.recv(1024)
     print ('Connected ' + addr[0] + ':' + str(addr[1]))
-    client_socket.sendall(b'Welcome to Server!')
+    client_socket.sendall(b'Welcome to Server! nakonec poluchilos')
     client_socket.close()
